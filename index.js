7077608f6ba9801bd42c47a56b5d8ef64b6dc7ea
Aiden Isaia <aidenisaia@gmail.com>
2021-07-14 18:25:04 -0700

    babel

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ console.log("Hello");
 console.log(moment().startOf("day").fromNow());
 console.log(moment().format("LTS"));
 
-var mom;
+var math = require("mathjs");
+
+console.log("create and manipulate complex numbers");
+const a = math.complex(2, 3);
+console.log(a); // 2 + 3i
+
+// Lexical arguments
+function square() {
+  let example = () => {
+    let numbers = [];
+    for (let number of arguments) {
+      numbers.push(number * number);
+    }
+
+    return numbers;
+  };
+
+  return example();
+}
+
+console.log(square(2, 4, 7.5, 8, 11.5, 21)); // returns: [4, 16, 56.25, 64, 132.25, 441]
